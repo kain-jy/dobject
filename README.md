1ea4089d0ae2ea969351dd69390c8768dff2272c
kain <me@kain-jy.com>
2015-02-20 03:58:54 +0900

    Init

---
+++ README.md
@@ -0,0 +1,1 @@
+# DObject - DDD model for Python
---
+++ setup.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+from setuptools import setup
+
+
+setup(
+    name='dobject',
+    version='0.0.1',
+    packages=['dobject'],
+    tests_require=[
+        'pytest>=2.6.4'
+    ]
+)
